cd905e1d3cba9af1e131eb89acccf2e234ce3db2
Robert Overs <oversr14@gmail.com>
2023-04-23 23:25:16 +1000

    update gitsigns

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -5,8 +5,9 @@ gitsigns.setup({
     add          = { text = '+' },
     change       = { text = '~' },
     delete       = { text = '-' },
-    topdelete    = { text = '‾' },
+    topdelete    = { text = '-' },
     changedelete = { text = '~' },
+
     untracked    = { text = '*' },
   },
   signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
