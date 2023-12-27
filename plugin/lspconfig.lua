639d750e2b93612f201b483b786b7a959183b41f
Robert Overs <oversr14@gmail.com>
2023-12-27 14:02:06 +1100

    Config updates

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -39,5 +39,24 @@ lsp.rust_analyzer.setup {
 
 lsp.pylsp.setup {
     on_attach = on_attach,
-    capabilities = capabilities
+    capabilities = capabilities,
+    settings = {
+      pylsp = {
+        plugins = {
+          -- linter options
+          flake8 = { enabled = true, maxLineLength = 120 },
+          pylint = { enabled = false },
+          pyflakes = { enabled = false },
+          pycodestyle = { enabled = false },
+          -- type checker
+          pylsp_mypy = { enabled = true },
+          -- auto-completion options
+          jedi_completion = { fuzzy = true },
+          -- import sorting
+          pyls_isort = { enabled = true },
+        }
+      }
+    }
 }
+
+vim.api.nvim_set_keymap("n", "<leader>a", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true })
